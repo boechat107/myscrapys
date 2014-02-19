e588183b07a8c3c887522fc2db4fd6b15f70b555
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-02-19 15:37:48 -0300

    First commit

---
+++ myscrapys/settings.py
@@ -0,0 +1,15 @@
+# Scrapy settings for myscrapys project
+#
+# For simplicity, this file contains only the most important settings by
+# default. All the other settings are documented here:
+#
+#     http://doc.scrapy.org/en/latest/topics/settings.html
+#
+
+BOT_NAME = 'myscrapys'
+
+SPIDER_MODULES = ['myscrapys.spiders']
+NEWSPIDER_MODULE = 'myscrapys.spiders'
+
+# Crawl responsibly by identifying yourself (and your website) on the user-agent
+#USER_AGENT = 'myscrapys (+http://www.yourdomain.com)'
