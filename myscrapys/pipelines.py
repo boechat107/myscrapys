165a6a5422a826855ff952150056721887f63820
Andre Ambrosio Boechat <boechat107@gmail.com>
2014-03-06 16:16:35 -0300

    Pipeline class to clean the parsed data
    * TODO: remove blank list items.

--- myscrapys/pipelines.py
+++ myscrapys/pipelines.py
@@ -3,6 +3,25 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: http://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
-class MyscrapysPipeline(object):
+
+class StringCleanerPipeline(object):
+    """
+    Prepares string values, like removing unnecessary white spaces.
+    """
+
+    def clean(self, value):
+        if isinstance(value, list):
+            outlist = []
+            for v in value:
+                outlist.append(self.clean(v))
+            return outlist
+        ## The method extract() always returns an unicode string.
+        elif isinstance(value, str) or isinstance(value, unicode):
+            return value.strip()
+        else:
+            return value
+
     def process_item(self, item, spider):
+        for k, v in item.iteritems():
+            item[k] = self.clean(v)
         return item
--- myscrapys/settings.py
+++ myscrapys/settings.py
@@ -12,8 +12,13 @@ SPIDER_MODULES = ['myscrapys.spiders']
 NEWSPIDER_MODULE = 'myscrapys.spiders'
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'myscrapys (+http://www.yourdomain.com)'
+USER_AGENT = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:25.0) Gecko/20100101 Firefox/25.0'
 
 ## My configuration
 AUTOTHROTTLE_ENABLED = True
 AUTOTHROTTLE_DEBUG = True
+
+## Pipelines
+ITEM_PIPELINES = {
+    'myscrapys.pipelines.StringCleanerPipeline': 300
+}
